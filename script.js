380fa0c9b5ccd3b90ca5169727a3a4ffc8be938c
Catherine <catherine.mathenge@strathmore.edu>
2023-07-05 11:03:11 +0300

    Done with assignment

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(response => response.json())
     .then(data => {
       const films = data.films;
-      films.forEach(film => {
+      films.forEach((film, index) => {
         const filmElement = document.createElement("div");
         filmElement.classList.add("film");
         filmElement.textContent = film.title;
@@ -38,6 +38,12 @@ document.addEventListener("DOMContentLoaded", () => {
           const posterElement = document.createElement("img");
           posterElement.src = film.poster;
 
+          const buyTicketButton = document.createElement("button");
+          buyTicketButton.textContent = "Buy Ticket";
+          buyTicketButton.addEventListener("click", () => {
+            buyMovieTicket(film, ticketsSoldElement, capacityElement);
+          });
+
           // Append film details to the container
           filmDetailsContainer.appendChild(titleElement);
           filmDetailsContainer.appendChild(runtimeElement);
@@ -46,13 +52,37 @@ document.addEventListener("DOMContentLoaded", () => {
           filmDetailsContainer.appendChild(ticketsSoldElement);
           filmDetailsContainer.appendChild(descriptionElement);
           filmDetailsContainer.appendChild(posterElement);
+          filmDetailsContainer.appendChild(buyTicketButton);
         });
 
         // Append film to the film list
         filmListContainer.appendChild(filmElement);
+
+        // Display details of the first movie automatically
+        if (index === 0) {
+          filmElement.click();
+        }
       });
+
+      function buyMovieTicket(film, ticketsSoldElement, buyTicketButton) {
+        if (film.tickets_sold < film.capacity) {
+          film.tickets_sold++;
+          ticketsSoldElement.textContent = `Tickets Sold: ${film.tickets_sold}`;
+          console.log(`You have successfully bought a ticket for "${film.title}". Enjoy the show!`);
+      
+          if (film.tickets_sold >= film.capacity) {
+            buyTicketButton.disabled = true;
+            prompt(`Sorry, the movie "${film.title}" is sold out.`);
+          }
+        } else {
+          prompt(`Sorry, the movie "${film.title}" is sold out.`);
+        }
+      }
+      
+      
     })
     .catch(error => {
       console.error("Error fetching films:", error);
     });
+    
 });
